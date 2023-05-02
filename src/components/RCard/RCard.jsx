a2e81314a84b5e569c9bbccb12f42d5e82b58643
asif93-138 <asif93@student.sust.edu>
2023-05-02 15:52:23 +0600

    toast added

--- src/components/RCard/RCard.jsx
+++ src/components/RCard/RCard.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Toast from 'react-bootstrap/Toast';
 
 const RCard = (props) => {
+    const [show, setShow] = useState(false);
+
     const {id, image, name, ingredients, cookingmethod} = props.recipe;
     return (
         <div>
             {id}, {image}, {name}, {ingredients}, {cookingmethod}
+            <Row>
+            <Col xs={6}>
+        <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Toast!</strong>
+            
+          </Toast.Header>
+          <Toast.Body>Added to favourites!</Toast.Body>
+        </Toast>
+      </Col>
+      <Col xs={6}>
+        <Button onClick={() => setShow(true)}>Favourite</Button>
+      </Col>
+            </Row>
         </div>
     );
 };
