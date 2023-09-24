7e6e0a1ac5cda8e6f7160e4dc5150113b968ba6c
asif93-138 <asif93@student.sust.edu>
2023-09-24 23:55:22 +0600

    navbar almost done!

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <div>
       <Header />
-      <Outlet />
-      <Footer />
+      {/* <Outlet />
+      <Footer /> */}
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,45 @@
 import React, { useContext } from 'react';
 import './Header.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+
+
 import { Link } from "react-router-dom";
 import ActiveLink from '../ActiveLink/ActiveLink';
 import { AuthContext } from '../../providers/AuthProvider';
 import { signedout } from '../LogIn/LogIn';
+import logo from '../../../public/chinese-utensils-riou.jpg';
 
 
 const Header = () => {
     const {loggedinuser} = useContext(AuthContext);
     return (
         <>
-        <Navbar className='nav-p' collapseOnSelect expand="md" bg="light" variant="light">
-        
-          <Nav className="">
-          <Navbar.Brand>Taste of China</Navbar.Brand>
-              <ActiveLink className='nav-link' to='/'>Home</ActiveLink>
-              <ActiveLink className='nav-link' to='/blog'>Blog</ActiveLink>
-              
-              
-              <ActiveLink className='nav-link' to='/protected'>Protected</ActiveLink>
-              {loggedinuser ? <><Link className='nav-link' onClick={signedout}>Log out</Link><img title={loggedinuser?.displayName} src={loggedinuser.photoURL} className='w-fix rounded-circle ms-1' /></>:<ActiveLink className='nav-link' to='/login'>Login</ActiveLink>}
-          </Nav>
-        </Navbar>
+          <nav className="navbar navbar-expand-sm bg-light navbar-light">
+          <div className="container-fluid">
+          <img src={logo} className='w-logo mx-md-2' />
+          <ActiveLink className="navbar-brand" to=""><h2 className='m-0'><strong>Taste of China</strong></h2></ActiveLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+          <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="mynavbar">
+          <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+          <ActiveLink className='nav-link' to='/'>Home</ActiveLink>
+          </li>
+          <li className="nav-item">
+          <ActiveLink className='nav-link' to='/blog'>Blog</ActiveLink>
+          </li>
+          <li className="nav-item">
+          <ActiveLink className='nav-link' to='/protected'>Protected</ActiveLink>
+          </li>
+          <li className="nav-item">
+          {loggedinuser ? <><Link className='nav-link' onClick={signedout}>Log out</Link><img title={loggedinuser?.displayName} src={loggedinuser.photoURL} className='w-fix rounded-circle ms-1' /></>:<ActiveLink className='nav-link' to='/login'>Login</ActiveLink>}
+          </li>
+          </ul>
+      
+          </div>
+          </div>
+          </nav>
+          
       </>
     );
 };
